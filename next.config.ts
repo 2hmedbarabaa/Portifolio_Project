e4a18192d083fe0233a5c2ee8b577093370b6fe4
Ahmed Salah Barabaa <ahmedbarabaa3@gmail.com>
2025-01-26 20:55:29 +0300

    updateCode

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Static export
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
+};
+
+export default nextConfig;
